6278c1c2ac512596c8b625a8ed6ac96959c4e9ae
yarikpopoff <yarikpopoff@gmail.com>
2017-01-10 22:12:18 +0200

    fourth

--- src/index.js
+++ src/index.js
@@ -1,12 +1,29 @@
 import ReactDOM from 'react-dom';
-//var ReactDOM = require('react-dom');
 import React from 'react';
-// var React = require('react');
 import NotesApp from './components/NotesApp.jsx';
-// var NotesApp = require('./components/NotesApp.jsx');
 import Button from 'react-bootstrap';
+import ButtonToolbar from 'react-bootstrap';
 
-ReactDOM.render(
-    <NotesApp />,
-    document.getElementById('mount-point')
+// ReactDOM.render(<NotesApp />, document.getElementById('mount-point'));
+const buttonsInstance = (
+  <div>
+    <ButtonToolbar>
+      <Button bsStyle="primary" bsSize="large">Large button</Button>
+      <Button bsSize="large">Large button</Button>
+    </ButtonToolbar>
+    <ButtonToolbar>
+      <Button bsStyle="primary">Default button</Button>
+      <Button>Default button</Button>
+    </ButtonToolbar>
+    <ButtonToolbar>
+      <Button bsStyle="primary" bsSize="small">Small button</Button>
+      <Button bsSize="small">Small button</Button>
+    </ButtonToolbar>
+    <ButtonToolbar>
+      <Button bsStyle="primary" bsSize="xsmall">Extra small button</Button>
+      <Button bsSize="xsmall">Extra small button</Button>
+    </ButtonToolbar>
+  </div>
 );
+
+ReactDOM.render(buttonsInstance, document.getElementById('mount-point'));
