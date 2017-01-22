a2a78caa33562d848ba5fe1032f2263aec72813e
yarikpopoff <yarikpopoff@gmail.com>
2017-01-22 19:00:38 +0200

    added the add a new row, but got stuck on removing the existing row

--- src/components/CostContainer.js
+++ src/components/CostContainer.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Alert } from 'react-bootstrap';
-import { ListGroup } from 'react-bootstrap';
-import { ListGroupItem } from 'react-bootstrap';
 import { Table } from 'react-bootstrap';
 
 require('./CostContainer.css');
@@ -12,14 +10,97 @@ export default class CostContainer extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            totalAmount: 0
+            totalAmount: 0,
+            expendituresArray: [],
+            expendituresArray1: [
+                {
+                    id: 0,
+                    date: '22.01.2017', // toLocaleDateString()
+                    description: 'Food',
+                    amount: -246.59
+                },
+                {
+                    id: 1,
+                    date: '22.01.2017',
+                    description: 'Salary',
+                    amount: 500.00
+                },
+                {
+                    id: 2,
+                    date: '22.01.2017',
+                    description: 'Rent',
+                    amount: -158.25
+                },
+                {
+                    id: 3,
+                    date: '22.01.2017',
+                    description: 'kindergarten',
+                    amount: -81.50
+                }
+            ] 
         };
     }
 
-    alertClicked(event) {
-        console.log('You clicked the Table');
-        console.log(event);
+    componentWillMount() {
+		
+        const calcSum=(amount, arr)=> {
+            if (arr) {
+                const sum = arr.reduce((sum, el)=>{
+                    return sum + el.amount;
+                }, amount);
+                return (Math.round(sum * 100) / 100);
+            } else {
+                return 0;
+            }
+        }
+
+        const localExpendituresArray = JSON.parse(localStorage.getItem('expendituresArray'));
+        if (localExpendituresArray) {
+            this.setState({ 
+                totalAmount: calcSum(this.state.totalAmount, localExpendituresArray), 
+                expendituresArray: localExpendituresArray 
+            });
+        } else {
+            if (this.state.expendituresArray) {
+                this.setState({ 
+                    totalAmount: calcSum(this.state.totalAmount, this.state.expendituresArray) 
+                });
+            }
+        };
     }
+
+    componentDidUpdate() {
+        this._updateLocalStorage();
+    }
+
+    pressDel=(e)=> {
+        console.log('You clicked the Del');
+        debugger;
+    }
+
+    addNew=()=> {
+        let newRow = {
+            id: Math.random().toString().slice(-10),
+            date: new Date().toLocaleDateString(),
+            description: 'new',
+            amount: 0
+        };
+        let newExpendituresArray = this.state.expendituresArray.slice();
+        newExpendituresArray.push(newRow);
+        this.setState({expendituresArray: newExpendituresArray});
+    }
+
+    _updateLocalStorage=()=> {
+		let localExpendituresArray = JSON.stringify(this.state.expendituresArray);
+		localStorage.setItem('expendituresArray', localExpendituresArray);	
+	}
+
+	_getFromLocalStorage=()=> {
+		let localExpendituresArray = JSON.parse(localStorage.getItem('expendituresArray'));
+		if (localExpendituresArray) {
+			this.setState({ expendituresArray: localExpendituresArray });
+		};
+	}
 
     render() {
 
@@ -31,38 +112,28 @@ export default class CostContainer extends React.Component {
                 <Table responsive>
                     <thead>
                         <tr>
-                            <th>#</th>
                             <th>Date</th>
                             <th>Description</th>
                             <th>Amount</th>
                             <th></th>
                         </tr>
                     </thead>
-                    <tbody onClick={this.alertClicked}>
-                        <tr>
-                            <td>1</td>
-                            <td>21/01/2017</td>
-                            <td>Food</td>
-                            <td>-246.59</td>
-                            <td><a href="#">&times;</a></td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>21/01/2017</td>
-                            <td>Salary</td>
-                            <td>500</td>
-                            <td>&times;</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>21/01/2017</td>
-                            <td>Rent</td>
-                            <td>-158.25</td>
-                            <td>&times;</td>
-                        </tr>
+                    <tbody>
+                        {
+                            this.state.expendituresArray.map((el,i)=> {
+                                return (
+                                    <tr key = { i }>
+                                        <td>{el.date}</td>
+                                        <td>{el.description}</td>
+                                        <td>{el.amount}</td>
+                                        <td className="delRow" onClick={this.pressDel}>&times;</td>
+                                    </tr>
+                                )
+                            })
+                        }
                     </tbody>
                 </Table>
-                <Button bsStyle="primary" block>Add new income or expense</Button>
+                <Button bsStyle="primary" block onClick={this.addNew}>Add new income or expense</Button>
             </div>
         )
     }
--- webpack.config.js
+++ webpack.config.js
@@ -9,41 +9,18 @@ module.exports = {
     },
     module: {
         loaders: [
-{
-                test: /\.js$/,
-                loader: "babel-loader",
-                exclude: [/node_modules/, /public/]
-            },
             {
-                test: /\.css$/,
-                loader: "style-loader!css-loader!autoprefixer-loader",
-                exclude: [/node_modules/, /public/]
+                test: /\.jsx?$/,
+                exclude: /(node_modules|bower_components)/,
+                loader: 'babel-loader',
+                query: {
+                    presets: ['react', 'es2015', 'stage-0'],
+                    plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy'],
+                },
             },
-            {
-                test: /\.gif$/,
-                loader: "url-loader?limit=10000&mimetype=image/gif"
-            },
-            {
-                test: /\.jpg$/,
-                loader: "url-loader?limit=10000&mimetype=image/jpg"
-            },
-            {
-                test: /\.png$/,
-                loader: "url-loader?limit=10000&mimetype=image/png"
-            },
-            {
-                test: /\.svg/,
-                loader: "url-loader?limit=26000&mimetype=image/svg+xml"
-            },
-            {
-                test: /\.jsx$/,
-                loader: "react-hot!babel-loader",
-                exclude: [/node_modules/, /public/]
-            },
-            {
-                test: /\.json$/,
-                loader: "json-loader"
-            }
+            { test: /\.scss$/, loader: 'style!css!sass' },
+            { test: /\.css$/, loader: "style!css" },
+            { test: /\.html$/, loader: 'html' }
         ]
     }
 }
